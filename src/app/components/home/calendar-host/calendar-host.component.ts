8ca2444ebbc4c310c62d757482239ce1d60e1c72
ramos29pages <=>
2024-06-10 18:16:31 -0500

    Fix: Calendar component

--- src/app/components/home/calendar-host/calendar-host.component.ts
+++ src/app/components/home/calendar-host/calendar-host.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { CalendarComponent } from './calendar/calendar.component';
+import { Component, OnInit, signal, WritableSignal } from '@angular/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
@@ -12,75 +13,11 @@ import { ThisReceiver } from '@angular/compiler';
   templateUrl: './calendar-host.component.html',
   styleUrls: ['./calendar-host.component.scss'],
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatInputModule,
-    MatButtonModule
-  ]
+  imports: [CommonModule, FormsModule, CalendarComponent],
 })
-export class CalendarHostComponent implements OnInit {
+export class CalendarHostComponent {
 
-  selectedDate: Date = new Date();
-  selectedMonth : number = 1;
-  selectedYear : number = 2024;
+  year: WritableSignal<number> = signal(new Date().getFullYear());
+  month: WritableSignal<number> = signal(new Date().getMonth() + 1);
 
-  ngOnInit(): void {
-    this.generateCalendar(this.selectedYear, this.selectedMonth);
-  }
-
-  updateDateInfo(): void {
-    this.selectedMonth = this.selectedDate.getMonth();
-    this.selectedYear = this.selectedDate.getFullYear();
-    console.log(this.selectedYear);
-  }
-
-  generateCalendar(year: number, month: number): number[][] {
-    const firstDayOfMonth = new Date(year, month, 1).getDay();
-    const daysInMonth = new Date(year, month + 1, 0).getDate(); // Get the last day of the month
-  
-    const calendar: number[][] = [];
-    let week: number[] = [];
-    let day = 1;
-  
-    // Add empty days before the first day of the month
-    for (let i = 0; i < firstDayOfMonth; i++) {
-      week.push(0);
-    }
-  
-    // Loop through all days in the month
-    while (day <= daysInMonth) {
-      week.push(day);
-      if (week.length === 7) {
-        calendar.push(week);
-        week = [];
-      }
-      day++;
-    }
-  
-    // Add remaining empty days to complete the last week
-    while (week.length < 7) {
-      week.push(0);
-    }
-    calendar.push(week);
-
-    console.log(calendar);
-  
-    return calendar;
-  }
-  
-
-  prevMonth(): void {
-    const currentMonth = this.selectedDate.getMonth();
-    this.selectedDate = new Date(this.selectedDate.setMonth(currentMonth - 1));
-    this.updateDateInfo();
-  }
-
-  nextMonth(): void {
-    const currentMonth = this.selectedDate.getMonth();
-    this.selectedDate = new Date(this.selectedDate.setMonth(currentMonth + 1));
-    this.updateDateInfo();
-  }
 }
--- src/app/components/home/calendar-host/calendar/calendar.component.ts
+++ src/app/components/home/calendar-host/calendar/calendar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Signal, WritableSignal, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
@@ -8,43 +8,86 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
-export class CalendarComponent implements OnInit {
-  @Input() month !: number;
-  @Input() year !: number;
 
-  weeks !: number[][];
+export class CalendarComponent implements OnInit {
+  @Input() year !: WritableSignal<number>;
+  @Input() month !: WritableSignal<number>;
+  _WEEKDAYS = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
+  calendar !: Signal<string[][]>;
+  monthName !: Signal<string>;
 
   ngOnInit(): void {
-    this.generateCalendar();
+    this.monthName = computed(() => this.showMonth(this.month()));
+    this.calendar = computed(() => this.createCalendar(this.year(), this.month()));
   }
 
-  generateCalendar(): void {
-    const date = new Date(this.year, this.month - 1, 1);
-    console.log('DATE REGISTERED:::', date);
-    const daysInMonth = new Date(this.year, this.month, 0).getDate();
-    console.log('MONTH REGISTERED:::', daysInMonth);
-    const startingDay = date.getDay();
-    console.log('DAY REGISTERED:::',startingDay);
+  createCalendar(year: number, month: number): string[][] {
+    let mon = month - 1; // Los meses en JS son 0..11, no 1..12
+    let d = new Date(year, mon);
+    let table: string[][] = [];
 
-    this.weeks = [];
-    let week = [];
+    let row: string[] = [];
+    // Espacios en la primera línea desde lunes hasta el primer día del mes
+    for (let i = 0; i < this.getDay(d); i++) {
+      row.push('');
+    }
 
-    for (let i = 0; i < daysInMonth; i++) {
-      const day = i + 1 - startingDay;
+    // <td> con el día  (1 - 31)
+    while (d.getMonth() === mon) {
+      row.push(d.getDate().toString());
 
-      if (day > 0 && day <= 7) {
-        week.push(day);
-      } else {
-        if (week.length) {
-          this.weeks.push(week);
-        }
-        week = [];
-        week.push(day);
+      if (this.getDay(d) % 7 === 6) { // domingo, último día de la semana --> nueva línea
+        table.push(row);
+        row = [];
+      }
+
+      d.setDate(d.getDate() + 1);
+    }
+
+    // Espacios después del último día del mes hasta completar la última línea
+    if (this.getDay(d) !== 0) {
+      for (let i = this.getDay(d); i < 7; i++) {
+        row.push('');
       }
     }
 
-    if (week.length) {
-      this.weeks.push(week);
+    if (row.length > 0) {
+      table.push(row);
+    }
+
+    return table;
+  }
+
+  getDay(date: Date): number { // obtener el número de día desde 0 (lunes) a 6 (domingo)
+    let day = date.getDay();
+    if (day === 0) day = 7; // hacer domingo (0) el último día
+    return day - 1;
+  }
+
+  showMonth(position: number): string {
+    const months = [
+      { position: 1, name: 'Enero' }, { position: 2, name: 'Febrero' },
+      { position: 3, name: 'Marzo' }, { position: 4, name: 'Abril' },
+      { position: 5, name: 'Mayo' }, { position: 6, name: 'Junio' },
+      { position: 7, name: 'Julio' }, { position: 8, name: 'Agosto' },
+      { position: 9, name: 'Septiembre' }, { position: 10, name: 'Octubre' },
+      { position: 11, name: 'Noviembre' }, { position: 12, name: 'Diciembre' }
+    ];
+
+    const month = months.find(m => m.position === position);
+    return month ? month.name : '';
+  }
+
+  changeMonth(direction: number): void {
+    const newMonth = this.month() + direction;
+    if (newMonth < 1) {
+      this.month.set(12);
+      this.year.set(this.year() - 1);
+    } else if (newMonth > 12) {
+      this.month.set(1);
+      this.year.set(this.year() + 1);
+    } else {
+      this.month.set(newMonth);
     }
   }
 }
