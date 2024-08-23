9644937ed5e998ad742d0045cfcd4fc4b18ad454
dev.ramos.pages@gmail.com <=>
2024-08-23 08:51:44 -0500

    fix(styles): remove unnesasary styles and fix menu button component

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -3,7 +3,7 @@ export const CURRENT_TOKEN : string = "CURRENT_TOKEN";
 export const api = {
   AuthLoginEndpoint : 'http://localhost:3000/api/v1/auth/login',
   AuthValidateUsernameEndpoint : 'http://localhost:3000/api/v1/auth/validate-username',
-  user : {
-    get: 'https://api.escuelajs.co/api/v1/auth/login',
+  users : {
+    getAllEndpoint: 'http://localhost:3000/api/v1/users',
   }
 }
